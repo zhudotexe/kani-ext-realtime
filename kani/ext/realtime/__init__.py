055a1eb0c518b41327891691c774a46620dddf89
Andrew Zhu <me@andrew-zhu.com>
2025-01-31 12:41:58 -0500

    fix: missing refactored imports

--- kani/ext/realtime/__init__.py
+++ kani/ext/realtime/__init__.py
@@ -1,4 +1,2 @@
-from .audio import get_audio_stream, list_mics, play_audio
 from .cli import chat_in_terminal_audio, chat_in_terminal_audio_async
 from .engine import OpenAIRealtimeKani
-from .models import ResponseConfig
